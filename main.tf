f83fc2df5a943577e884705b2638c8b79776c02b
ilovedragon <ngpohleng@yahoo.com>
2023-07-25 21:54:11 +0800

    Modify code to use reference name for sqs arn

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 #Reference URL : https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_notification#add-notification-configuration-to-sqs-queue
 
+#Create the queuce(without the policy)
+resource "aws_sqs_queue" "queue" {
+  name   = "s3-event-notification-queue-for-pohleng" #change name here
+#  policy = data.aws_iam_policy_document.queue.json
+}
+
+#Create the policy, and the resource would reference the created queue.
 data "aws_iam_policy_document" "queue" {
   statement {
     effect = "Allow"
@@ -10,8 +17,8 @@ data "aws_iam_policy_document" "queue" {
     }
 
     actions   = ["sqs:SendMessage"]
-    resources = ["arn:aws:sqs:*:*:s3-event-notification-queue-for-pohleng"] #change name here
-
+#    resources = ["arn:aws:sqs:*:*:s3-event-notification-queue-for-pohleng"] #change name here
+     resources = [aws_sqs_queue.queue.arn] #change name here
     condition {
       test     = "ArnEquals"
       variable = "aws:SourceArn"
@@ -20,9 +27,10 @@ data "aws_iam_policy_document" "queue" {
   }
 }
 
-resource "aws_sqs_queue" "queue" {
-  name   = "s3-event-notification-queue-for-pohleng" #change name here
-  policy = data.aws_iam_policy_document.queue.json
+#Attached the policy to the queue
+resource "aws_sqs_queue_policy" "queue_policy" {
+  queue_url = aws_sqs_queue.queue.id
+  policy    = data.aws_iam_policy_document.queue.json
 }
 
 resource "aws_s3_bucket" "bucket" {
